ac9be14d3cdabcf7d1faac4848e3c0aeed2a33be
Your Name <you@example.com>
2024-10-14 11:31:40 +0600

    backup

--- ranker/difficulties/gpt.py
+++ ranker/difficulties/gpt.py
@@ -5,21 +5,21 @@ class DifficultyGPTCompletion(GeminiGPTCompletion):
     system_instruction = """
     You are a difficulty detector.
 
-    Note: response should be only the difficulty score.
+    Note: response should be only the difficulty score (in Float).
     no extra spaces and talks.
 
     Suggest difficulty for the challenge.
-    You will be provided score range of difficulty (float not allowed),
+    You will be provided score range of difficulty,
     info of the user, previously completed challenges with their
     difficulty score and the challenge thats difficulty to be detected.
 
-    example response: 5
+    example response: 5.203
     """
 
     fallback_result = 2
 
     def clean_result(self, result):
-        return int(result.strip())
+        return round(float(result.strip()) - 0.5)
 
     def is_valid_result(self, result):
         return isinstance(result, int)
